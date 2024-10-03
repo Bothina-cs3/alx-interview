888f26b4a6e88e2b1c45b3a5542ec604d243f944
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-10-03 21:12:55 +0000

    by bothina

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,17 +2,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    triangle = [[1]]  # First row of Pascal's Triangle
 
     for i in range(1, n):
-
-        row = [1]
-
+        row = [1]  # Start with 1 for the current row
         for j in range(1, i):
+            # Calculate the value based on the previous row
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-
-        row.append(1)
-
-        triangle.append(row)
+        row.append(1)  # End with 1
+        triangle.append(row)  # Append the completed row to the triangle
 
     return triangle
